c57fa3837f3bff619942b18908465c4a37e6bbf9
”taka292” <12takao07@gmail.com>
2020-07-04 16:51:28 +0000

    first commit

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,26 +1,14 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset= "utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>MyNews</title>
-        
-    </head>
-    <body>
-        <h1>Myニュース作成画面</h1>
-    </body>
-</html>
-
 @extends('layouts.admin')
-@section('title','ニュースの新規作成')
+@section('title', 'ニュースの新規作成')
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 mx-auto">
-            <h2>ニュース新規作成</h2>
-            <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>ニュース新規作成</h2>
+                <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
+
+ // 以下を追記
 
                     @if (count($errors) > 0)
                         <ul>
@@ -30,19 +18,19 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
+                        <label class="col-md-2" for="title">タイトル</label>
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">本文</label>
+                        <label class="col-md-2" for="body">本文</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">画像</label>
+                        <label class="col-md-2" for="title">画像</label>
                         <div class="col-md-10">
                             <input type="file" class="form-control-file" name="image">
                         </div>
@@ -51,7 +39,6 @@
                     <input type="submit" class="btn btn-primary" value="更新">
                 </form>
             </div>
-        
+        </div>
     </div>
-</div>
 @endsection
