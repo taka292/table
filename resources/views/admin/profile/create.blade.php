676af7c004facf5edcc8f4520b58bea55ad4643b
”taka292” <12takao07@gmail.com>
2020-07-04 11:03:35 +0000

    first commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset= "utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Myprofile</title>
+    </head>
+    <body>
+        <h1>Myprofile作成画面</h1>
+        <a href="https://b99ade87a77f48958138163c448ead3e.vfs.cloud9.us-east-2.amazonaws.com/admin/profile/edit">編集する</a>
+    </body>
+</html>
+
+@extends('layouts.profile')
+@section('title','profileの新規作成')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <h2>Myprofileの新規作成</h2>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset= "utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Myprofile</title>
+    </head>
+    <body>
+        <h1>MYprofile編集画面</h1>
+        <a href="https://b99ade87a77f48958138163c448ead3e.vfs.cloud9.us-east-2.amazonaws.com/admin/profile/create">作る</a>
+    </body>
+</html>
