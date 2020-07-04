3139addb093dcc5bc59403ff3039585cefd36e4f
”taka292” <12takao07@gmail.com>
2020-07-04 12:46:23 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,18 @@ Route::group(['prefix' => 'admin'], function() {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); 
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    //  Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::post('profile/create', 'Admin\ProfileController@create'); 
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    //  Route::get('profile/edit', 'Admin\ProfileController@add');
+     Route::post('profile/edit', 'Admin\ProfileController@update'); 
+});
